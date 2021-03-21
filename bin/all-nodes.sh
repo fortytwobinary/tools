81455b3518e636e836641386cf5412bd857f09c3
David L Whitehurst <dlwhitehurst@gmail.com>
2021-03-21 21:46:22 +0000

    chmod and cat tee

--- bin/all-nodes.sh
+++ bin/all-nodes.sh
@@ -7,10 +7,17 @@
 #
 
 # install docker
-sudo apt-get update && sudo apt install -y docker.io
+(  set -e
+   sudo apt-get update 
+)
+
+(  set -e 
+   sudo apt install -y docker.io
+)
 
 # configure cgroup driver to be systemd
-sudo cat > /etc/docker/daemon.json <<EOF
+(  set -e
+   cat <<EOF | sudo tee /etc/docker/daemon.json
 {
   "exec-opts": ["native.cgroupdriver=systemd"],
   "log-driver": "json-file",
@@ -20,28 +27,37 @@ sudo cat > /etc/docker/daemon.json <<EOF
   "storage-driver": "overlay2"
 }
 EOF
+)
 
 # now append memory and swap options to the end of cmdline.txt
+(  set -e
 sudo sed -i '$ s/$/ cgroup_enable=cpuset cgroup_enable=memory cgroup_memory=1 swapaccount=1/' /boot/firmware/cmdline.txt
+)
 
 # Kubernetes recommends iptables and iptables6 set for bridged-network traffic
-cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+(  set -e
+   cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
 net.bridge.bridge-nf-call-ip6tables = 1
 net.bridge.bridge-nf-call-iptables = 1
 EOF
+)
 
 # now activate
-sudo sysctl --system
+(  set -e
+   sudo sysctl --system
+)
 
 # add Google's repo key (packages.cloud.google.com)
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+(  set -e
+   curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 
 # add the Kubernetes repo to our list of repos
-cat <<EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
+   cat <<EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
 deb https://apt.kubernetes.io/ kubernetes-xenial main
 EOF
 
 # now install Kubernetes, kubelet, kubeadm, and kubectl
-sudo apt update && sudo apt install -y kubelet kubeadm kubectl
+  sudo apt update && sudo apt install -y kubelet kubeadm kubectl
 
-
+  sudo apt-mark hold kubelet kubeadm kubectl
+)
