ceb9e9ae21f49be3aa0d21914c70a3f0228b65bf
David L Whitehurst <dlwhitehurst@gmail.com>
2021-04-19 02:15:30 +0000

    update 2

---
+++ bin/pod-dump.sql
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+mysqldump wikijs > tmp/wikijs.sql 
--- bin/wikijs-backup.sh
+++ bin/wikijs-backup.sh
@@ -5,3 +5,8 @@ POD=$(kubectl get pod -n wikijs | grep wikijs-mariadb | awk '{print $1}')
 echo $POD
 echo $TIMESTAMP
 
+kubectl cp .my.cnf -n wikijs $POD:root/.my.cnf
+kubectl cp pod-dump.sql -n wikijs $POD:tmp/pod-dump.sql
+
+kubectl exec -it $POD -n wikijs -- bash
+
