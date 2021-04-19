e52d50807f627ecf46940a37b8bf6993eb5b1e71
David L. Whitehurst <dlwhitehurst@gmail.com>
2021-04-19 13:11:03 -0400

    remove debug echos

--- bin/wikijs-backup.sh
+++ bin/wikijs-backup.sh
@@ -2,8 +2,6 @@
 
 TIMESTAMP=$(date +"%Y%m%d_%H%M%S")
 POD=$(kubectl get pod -n wikijs | grep wikijs-mariadb | awk '{print $1}')
-echo $POD
-echo $TIMESTAMP
 
 kubectl cp .my.cnf -n wikijs $POD:root/.my.cnf
 kubectl cp pod-dump.sql -n wikijs $POD:tmp/pod-dump.sql
